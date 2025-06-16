2f2f1a91773642897b7ec6f808695ce94c50bb74
lanceethanong <160747400+lanceethanong@users.noreply.github.com>
2025-06-16 13:45:11 +0800

    Delete src directory

--- src/LoginBackground.jsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }) {
-  return (
-    <div className="text-white h-[100vh] flex justify-center items-center bg-cover" style={{ backgroundImage: "url('../src/assets/manila.png')" }}>
-      {children}
-    </div>
-  );
-}
