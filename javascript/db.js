f17a2ecb1332d5288f30fda9fc377dabf94df4e1
Joaquin Adam Perez <101857618+AdalinePertrude@users.noreply.github.com>
2025-07-04 16:29:56 +0800

    Add files via upload

--- javascript/db.js
+++ javascript/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb+srv://<password>-admin@cluster0.ndwtzr5.mongodb.net/lab_res_db?retryWrites=true&w=majority&appName=Cluster0';
+const uri = 'mongodb+srv://admin:<db_password>@cluster0.ndwtzr5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
 
 module.exports = () => mongoose.connect(uri);
