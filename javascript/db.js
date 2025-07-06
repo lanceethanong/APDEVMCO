cc1f2792c0831cc9821112be7c8c449573464eb6
lanceethanong <160747400+lanceethanong@users.noreply.github.com>
2025-07-06 13:04:11 +0800

    Delete javascript directory

--- javascript/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-const uri = 'mongodb+srv://admin:<db_password>@cluster0.ndwtzr5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
-
-module.exports = () => mongoose.connect(uri);
