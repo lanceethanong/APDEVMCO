c0af653f31c0065a68d6c97bb16f75d7d632a3bc
lanceethanong <160747400+lanceethanong@users.noreply.github.com>
2025-07-14 22:42:00 +0800

    Delete public/javascript/db.js

--- public/javascript/db.js
+++
@@ -1,5 +0,0 @@
-const mongoose = require('mongoose');
-
-const uri = 'mongodb+srv://<password>-admin@cluster0.ndwtzr5.mongodb.net/lab_res_db?retryWrites=true&w=majority&appName=Cluster0';
-
-module.exports = () => mongoose.connect(uri);
